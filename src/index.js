2aeef1c9d5b2819b1013fc246d0a00f2c209e547
Bruce Wayne <muditsjain@gmail.com>
2024-06-24 16:28:40 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const GumletEmbedPlayer = ({ 
+  videoID,
+  title="Gumlet video player",
+  style={padding:"56.25% 0 0 0", position:"relative"},
+  schemaOrgVideoObject={},
+  ...props
+}) => {
+  if(!videoID) return <div className={styles.error}>Error: videoID is required</div>
+
+  let srcURL = new URL(`https://play.gumlet.io/embed/${videoID}`);
+  
+  for (const [key, value] of Object.entries(props)) {
+    srcURL.searchParams.append(key, value);
+  }
+
+  return (
+    <div style={style}>
+      {
+        JSON.stringify(schemaOrgVideoObject) !== '{}' ? <script type='application/ld+json' dangerouslySetInnerHTML={ { __html: `${JSON.stringify(schemaOrgVideoObject)}`}} /> : null
+      }
+      
+      <iframe 
+        loading="lazy" 
+        title={title}
+        src={srcURL.toString()}
+        style={{border:"none", position: "absolute", "top":0, "left":0, "height": "100%", "width": "100%"}}
+        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen;"
+        allowFullScreen>
+      </iframe>
+    </div>
+  )
 }
